8daee0fbc8009dcc8391e8b53810f3c4ab47303b
fiandev <wongkeras2@gmail.com>
2023-11-08 05:35:19 +0700

    fix: CI/CD [vercel.json]

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     // depending on your application, base can also be "/"
     base: '/',
-    build: {
-      outDir: './build',
-    },
     plugins: [react()],
     server: {    
         // this ensures that the browser opens upon server start
